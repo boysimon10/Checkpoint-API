2a4cd6ef39cbe48e7142a9d6c223e4625b013920
boysimon10 <simondiouf2710@gmail.com>
2024-01-17 13:43:17 +0000

    final w style

--- src/UserList.js
+++ src/UserList.js
@@ -20,14 +20,25 @@ const UserList = () => {
     }, []);
 
 return (
-    <div className="user-list">
-        <h2>User List</h2>
+<div className="flex items-center justify-center">
+    <div className="bg-white shadow-md rounded-md p-6 w-96">
+        <h2 className="text-xl font-semibold mb-4">Tableau de bord</h2>
         <ul>
-        {listOfUsers.map(user => (
-            <li key={user.id}>{user.name}</li>
-        ))}
+            {listOfUsers.map(user => (
+                <li key={user.id} className="mb-2">
+                    <div className="flex items-center">
+                        <div className="w-8 h-8 bg-gray-300 rounded-full mr-2"></div>
+                        <div>
+                            <p className="text-gray-800 font-semibold">{user.name}</p>
+                            <p className="text-gray-600">Username: {user.username}</p>
+                            <p className="text-gray-600">Email: {user.email}</p>
+                        </div>
+                    </div>
+                </li>
+            ))}
         </ul>
     </div>
+</div>
 );
 };
 
